87cbc9e7c7eacc1598a564dccb358662d23428a1
VitalySVV <sy4evskij2014@gmail.com>
2025-03-16 21:17:10 +0300

    Initial commit

---
+++ src/components/JournalIteam/JournalIteam.jsx
@@ -0,0 +1,14 @@
+import './JournalIteam.css';
+function JournalIteam() {
+  return (
+    <div className='journal-item'>
+        <h2 className='journal-item__header'>1</h2>
+         <h2 className='journal-item__body'>
+            <div className='journal-item__dat'>2</div>
+            <div className='journal-item__text'>3</div>
+         </h2>
+    </div>
+  ); 
+}
+
+export default JournalIteam;
