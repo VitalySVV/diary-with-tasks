f322481d698f660ccd1d2e69a837930b22394496
VitalySVV <sy4evskij2014@gmail.com>
2025-03-17 22:13:31 +0300

    add CardButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
 import JournalIteam from './components/JournalIteam/JournalIteam';
 function App() {
-  return (
-    <>
-      <h1>Заголовок</h1>
-      <p>Какой-то текст</p>
-      <Button/>
-      <JournalIteam/>
-    </> 
-  ); 
+	const data = [
+		{
+			title: 'Подготовка и обновление курсов',
+			text: 'Горные походы открывают удивительные природные ландшафты',
+			date: new Date()
+		},
+		{
+			title: 'Походы в горы', 
+			text: 'Думал, что очень много времени',
+			date: new Date()
+		}
+	];
+
+	return (
+		<>
+			<h1>Заголовок</h1>
+			<p>Какой-то текст</p>
+			<Button/>
+			<CardButton>
+        Новое воспоминание
+			</CardButton>
+			<CardButton>
+				<JournalIteam
+					title = {data[0].title}
+					text = {data[0].text}
+					date = {data[0].date}
+				/>
+			</CardButton>
+			<CardButton>
+				<JournalIteam
+					title = {data[1].title}
+					text = {data[1].text}
+					date = {data[1].date}
+				/>
+			</CardButton>
+		</> 
+	); 
 }
 
 export default App;
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import './Button.css';
 function Button() {
-  return (
-    <button className='button accent'>Сохранить</button>
-  ); 
+	return (
+		<button className='button accent'>Сохранить</button>
+	); 
 }
 
 export default Button;
